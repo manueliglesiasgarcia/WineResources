68ec31811b4cc3059f6295e6fd3d841df95f95dd
mandruis7 <142666543+manueliglesiasgarcia@users.noreply.github.com>
2025-07-12 17:30:53 +0100

    Dynamically detect if there's a second IP if not use the first one

--- build/template/msbuild/start_uba.sh
+++ build/template/msbuild/start_uba.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
-HORDE_AGENT_IP=$(hostname -I | awk '{print $2}')
-HORDE_TOKEN=$(curl "$HORDE_SERVER_URL/api/v1/admin/registrationtoken")
+
+# Try to get the second IP address, fallback to the first if not available
+HORDE_AGENT_IP=$(hostname -I | awk '{print ($2 != "") ? $2 : $1}')
+
+# Get the registration token
+HORDE_TOKEN=$(curl -s "$HORDE_SERVER_URL/api/v1/admin/registrationtoken")
+
+# Export the values
 export HORDE_AGENT_IP=$HORDE_AGENT_IP
 export HORDE_TOKEN=$HORDE_TOKEN
+
+# Replace variables in the JSON template
 envsubst < /opt/horde-agent/appsettings.json.template > /opt/horde-agent/appsettings.json
+
+# Set server and run the agent
 dotnet /opt/horde-agent/HordeAgent.dll SetServer -Url="$HORDE_SERVER_URL" -Token="$HORDE_TOKEN" -Default
 dotnet /opt/horde-agent/HordeAgent.dll
- 
